50274c19d6503c6f14835b534d608da7629f091d
FedeGA2801 <federico2801@gmail.com>
2021-05-31 21:12:40 -0300

    Agrego migraciones

---
+++ FE/Migrations/202106010012101_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿namespace FE.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Afiliados",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        nombre = c.String(),
+                        apellido = c.String(),
+                        email = c.String(),
+                        telefono = c.String(),
+                        cuit = c.Long(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Afiliados");
+        }
+    }
+}
---
+++ FE/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace FE.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FE.Data.Services.PruebaDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FE.Data.Services.PruebaDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
